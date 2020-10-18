701bc3350a5be878ca289a38d684c35c76041cb1
Myriam Benkiran <myriambenkiran@MacBook-Air-de-Myriam.local>
2020-10-18 10:15:43 +0100

    Updates

---
+++ precache-manifest.b0954cb037a13b0fea86b831057957b3.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a72755325d734066e31f34801f76cd",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "8e5c80760975c19485ab",
+    "url": "/blog/static/css/2.69507427.chunk.css"
+  },
+  {
+    "revision": "a02184b11d4f14d11b5e",
+    "url": "/blog/static/css/main.9c064e3c.chunk.css"
+  },
+  {
+    "revision": "8e5c80760975c19485ab",
+    "url": "/blog/static/js/2.49d0cc59.chunk.js"
+  },
+  {
+    "revision": "be14b386ccc34a49dee76740150eacbc",
+    "url": "/blog/static/js/2.49d0cc59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a02184b11d4f14d11b5e",
+    "url": "/blog/static/js/main.6cf14849.chunk.js"
+  },
+  {
+    "revision": "042e0e1222fb6f3edc08",
+    "url": "/blog/static/js/runtime-main.1dc37962.js"
+  },
+  {
+    "revision": "7914f2428631844f8d24d689182a74d9",
+    "url": "/blog/static/media/1.7914f242.jpg"
+  },
+  {
+    "revision": "13e5b7ee3cfe277aa766690243d8429a",
+    "url": "/blog/static/media/2.13e5b7ee.jpg"
+  },
+  {
+    "revision": "619cc8f5b8fc6321dae72e7d96f55ca0",
+    "url": "/blog/static/media/3.619cc8f5.jpg"
+  },
+  {
+    "revision": "ab732d9f8b3a7a8838e33f1f67e3f57d",
+    "url": "/blog/static/media/4.ab732d9f.jpg"
+  },
+  {
+    "revision": "317d1eed89962e8115667fd7bcbe040c",
+    "url": "/blog/static/media/5.317d1eed.jpg"
+  },
+  {
+    "revision": "0832410a5d37a3cb210337ec1608e8df",
+    "url": "/blog/static/media/ins.0832410a.png"
+  },
+  {
+    "revision": "2df39009b341ae89d8a6000c1a130201",
+    "url": "/blog/static/media/ins2.2df39009.png"
+  },
+  {
+    "revision": "08d9911e4dce941ce7e9ba2c8b364892",
+    "url": "/blog/static/media/ins3.08d9911e.png"
+  },
+  {
+    "revision": "8290266a5fb402a34b96f890bbdb2d60",
+    "url": "/blog/static/media/insta.8290266a.png"
+  },
+  {
+    "revision": "30dde907d548b3ed07862f1227e21de3",
+    "url": "/blog/static/media/logo.30dde907.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blog/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5183ff13f41c9a908419ae2c48a3dc3d",
+    "url": "/blog/static/media/mibenki.5183ff13.png"
+  },
+  {
+    "revision": "71df0f234130872b63390f85c2871988",
+    "url": "/blog/static/media/outfit1.71df0f23.jpg"
+  },
+  {
+    "revision": "8d5709127e15055b4bd27caa85176916",
+    "url": "/blog/static/media/outfit2.8d570912.jpg"
+  },
+  {
+    "revision": "484e67b943bc788906bfb6496c16ae2d",
+    "url": "/blog/static/media/pin.484e67b9.jpg"
+  },
+  {
+    "revision": "40e26beab6a4049d7d52cee459a7dbb8",
+    "url": "/blog/static/media/pin2.40e26bea.png"
+  },
+  {
+    "revision": "73d6bf1e2a1389572f6cb5cc29405819",
+    "url": "/blog/static/media/pinterest.73d6bf1e.png"
+  }
+]);
